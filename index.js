d943f50cb11594335f4b1c0302251f069e23f768
Rishik <kushwaha202004@gmail.com>
2026-02-17 15:35:53 +0530

    Add experimental feature on arsh's branch

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ const fs = require('fs');
 
 // Simple logic to show we have code
 console.log("Backend Engineering Lab: Version Control Practical");
+
+
+// arsh's experimental feature
+console.log("Feature Test: Rahul's experimental code is running.");
